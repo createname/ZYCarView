bd3f963a7715f56ccb1c4bbae8a56d62b6b670d5
createname <zengyongeryu@163.com>
2018-03-08 16:19:40 +0800

    Initial Commit

---
+++ ZYCarView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYCarView
+//
+//  Created by liqiaona on 2018/3/8.
+//  Copyright © 2018年 ZY. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
